6b2e8534df898692a98ee5b4faa2ede0aaa8f58c
cuihaicheng <cuihaicheng@xueqiu.com>
2021-09-10 18:25:46 +0800

    debug: collection data

---
+++ src/main/java/io/haicheng/stockmonitorcollection/IndexController.java
@@ -0,0 +1,54 @@
+package io.haicheng.stockmonitorcollection;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * <p>Title: IndexController</p>
+ * <p>Description: </p>
+ * ----------------------------
+ *
+ * @author haicheng
+ * @date 2021/9/10 6:16 下午
+ */
+@RestController
+@RequestMapping("/v1")
+public class IndexController {
+
+    @Autowired
+    MeterRegistry registry;
+
+    private Counter counter_core;
+    private Counter counter_index;
+
+    @PostConstruct
+    private void init(){
+        counter_core = registry.counter("app_requests_method_count", "method", "IndexController.core");
+        counter_index = registry.counter("app_requests_method_count", "method", "IndexController.index");
+    }
+
+    @RequestMapping(value = "/index")
+    public Object index(){
+        try{
+            counter_index.increment();
+        } catch (Exception e) {
+            return e;
+        }
+        return counter_index.count() + " index of springboot2-prometheus.";
+    }
+
+    @RequestMapping(value = "/core")
+    public Object coreUrl(){
+        try{
+            counter_core.increment();
+        } catch (Exception e) {
+            return e;
+        }
+        return counter_core.count() + " coreUrl Monitor by Prometheus.";
+    }
+}
--- src/main/java/io/haicheng/stockmonitorcollection/StockMonitorCollectionApplication.java
+++ src/main/java/io/haicheng/stockmonitorcollection/StockMonitorCollectionApplication.java
@@ -1,7 +1,11 @@
 package io.haicheng.stockmonitorcollection;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -12,4 +16,9 @@ public class StockMonitorCollectionApplication {
         SpringApplication.run(StockMonitorCollectionApplication.class, args);
     }
 
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
